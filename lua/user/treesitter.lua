d209fa0e47b1724186bce5d0465409797afb73cf
Dash <dash@aladinfun.com>
2022-07-25 17:20:34 +0800

    tree-sitter only install languages I needed instead of "all"

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,31 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
+	ensure_installed = {
+		"html",
+		"css",
+		"c",
+		"c_sharp",
+		"cpp",
+		"vim",
+		"lua",
+		"javascript",
+		"typescript",
+		"tsx",
+		"go",
+		"gomod",
+		"gowork",
+		"rust",
+		"json",
+		"json5",
+		"make",
+		"markdown",
+		"proto",
+		"python",
+		"sql",
+		"toml",
+		"yaml",
+	}, -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
