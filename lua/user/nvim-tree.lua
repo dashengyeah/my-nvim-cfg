071dc0d299dd9098bb93c87e9bba32cd7e6e303e
Dash <dash@aladinfun.com>
2022-07-26 20:28:12 +0800

    set tabs

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -32,7 +32,7 @@ nvim_tree.setup {
           symlink_open = "",
         },
         git = {
-          unstaged = "",
+          unstaged = "M",
           staged = "S",
           unmerged = "",
           renamed = "➜",
